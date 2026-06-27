9c2676dafc951bb3cca5b0a05e4c64a16e527028
Anne Ferreira <ferreirane2509@gmail.com>
2026-06-27 13:08:44 -0300

    Delete Doações IFPR directory

--- Doações IFPR/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start(); 
-
-    session_unset(); // Apaga as variáveis da sessão
-    session_destroy(); // Destrói a sessão
-
-    header("Location: formLogin.php"); // Redireciona o usuário para o formulário de Login
-    exit();
-?>
